bbe84151efce71fa46d97122c3e9f15ba3ce4bfd
Danish-RS <danidesigner2012@gmail.com>
2024-04-01 03:11:50 +0500

    System Number Huessing Game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 8 + 1);
+const guessNumber = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Kindly_Guess_a_Number_between 1 to 8: ",
+    },
+]);
+if (guessNumber.userGuessedNumber === randomNumber) {
+    console.log("Congrates...! You Guess Right Number. ");
+}
+else {
+    console.log("Sorry...! You Guessed Wrong Number");
+}
